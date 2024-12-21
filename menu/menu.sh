a25073238e5dce19e3a30f53debdcc2990232222
fajrixbd <155747830+vhzv@users.noreply.github.com>
2024-12-21 16:59:04 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -290,7 +290,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " $COLOR1│${NC} ${COLBG1}               ${WH} •  VHZVPN PREMIUM TUNNELING •             ${NC} $COLOR1│ $NC"
+echo -e " $COLOR1│${NC} ${COLBG1}               ${WH} •  VHZVPN PREMIUM TUNNELING •           ${NC} $COLOR1│ $NC"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ OS            ${COLOR1}: ${WH}$MODEL2${NC}"
